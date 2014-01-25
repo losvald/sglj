c917c9158f03a144959cdc69681fe1aaf6d63459
Leo Osvald <leo.osvald@gmail.com>
2014-01-24 21:06:07 -0500

    Make DefaultMessagePublisher publish correctly in prepareAndPublish().

--- src/main/java/org/sglj/msg/DefaultMessagePublisher.java
+++ src/main/java/org/sglj/msg/DefaultMessagePublisher.java
@@ -1,20 +1,20 @@
 /*
  * DefaultMessagePublisher.java
- * 
+ *
  * Copyright (C) 2009 Leo Osvald <leo.osvald@gmail.com>
- * 
+ *
  * This file is part of SGLJ.
- * 
+ *
  * SGLJ is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * SGLJ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,24 +26,24 @@ import java.util.Vector;
 
 /**
  * Thread-safe implementation of {@link MessagePublisher}.
- * 
- * 
+ *
+ *
  * @author Leo Osvald
  *
  */
 public class DefaultMessagePublisher implements MessagePublisher {
 
 	private final Vector<MessageHandler> handlers = new Vector<MessageHandler>();
-	
+
 	private final StringBuffer[] messages;
-	
+
 	public DefaultMessagePublisher(int typeCount) {
 		messages = new StringBuffer[typeCount];
 		for(int i = 0; i < messages.length; ++i) {
 			messages[i] = new StringBuffer();
 		}
 	}
-	
+
 	@Override
 	public boolean cancel(MessageType type) {
 		synchronized (type) {
@@ -60,7 +60,7 @@ public class DefaultMessagePublisher implements MessagePublisher {
 	@Override
 	public MessagePublisher prepare(String chunk, MessageType type)
 			throws IndexOutOfBoundsException {
-		synchronized (type) {	
+		synchronized (type) {
 			messages[type.ordinal()].append(chunk);
 		}
 		return this;
@@ -68,7 +68,7 @@ public class DefaultMessagePublisher implements MessagePublisher {
 
 	@Override
 	public MessagePublisher prepareAndPublish(String msg, MessageType type) {
-		informHandlers(new Message(messages[type.ordinal()].toString(),
+		informHandlers(new Message(msg,
 				type, Calendar.getInstance().getTimeInMillis()));
 		return this;
 	}
@@ -99,8 +99,8 @@ public class DefaultMessagePublisher implements MessagePublisher {
 		for(int i = 0; i < handlers.size(); ++i)
 			handlers.get(i).messageReceived(msg);
 	}
-	
-	
+
+
 	protected static class StringBufferUtils {
 		public static boolean clear(StringBuffer buff) {
 			int len = buff.length();
@@ -108,5 +108,5 @@ public class DefaultMessagePublisher implements MessagePublisher {
 			return len > 0;
 		}
 	}
-	
+
 }
